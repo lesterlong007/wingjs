6e0f3847c3b40179c41abd2f85245411110024d8
lesterlong007 <longchb13@lzu.edu.cn>
2024-02-22 23:17:58 +0800

    initial files

---
+++ scripts/rollup.config.js
@@ -0,0 +1,8 @@
+export default {
+  input: "src/index.tsx", 
+  output: {
+    file: "dist/bundle.js",
+    format: "umd",
+    name: "MyBundle",
+  },
+};
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+
+
+export { copy } from './common/common';
+export { Toast } from  './common/common';
+export { setTitle } from  './common/common';
+export { parseSearch } from  './common/common';
+export { getQueryParam } from  './common/common';
+export { getClientWidth } from  './dom/dom';
+export { screenRate } from  './dom/dom';
+export { getRandomStr } from  './common/common';
+export { getCookie } from  './common/common';
+export { isMobile } from  './common/common';
+export { getDeviceType } from  './common/common';
